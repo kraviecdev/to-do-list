88b5ef20368e7dba934f74a16fdb1a01b650a3b5
Patryk Krawczyk <patrick36212@gmail.com>
2021-08-20 09:26:28 +0200

    added buttons - delete and done

--- js/script.js
+++ js/script.js
@@ -1,18 +1,32 @@
 {
-    const tasks = [];
+    const tasks = [
+      {
+        content: "test",
+      },
+      {
+        content: "test22",
+      },
+    ];
 
     const render = () => {
         let htmlString = "";
 
         for(const task of tasks) {
             htmlString += `
-            <li>
-                ${task.content}
+            <li class="list__item">
+               <button class="js-doneButton buttton button--done">Zrobione</button> ${task.content} <button class="js-deleteButton button button--delete">Usuń</button>
             </li>
             `
         };
-        document.querySelector("js-taskList").innerHTML = htmlString;
+        document.querySelector(".js-taskList").innerHTML = htmlString;
     };
+
+    const onFormSubmit = (event) => {
+      event.preventDefault();
+
+
+
+    }
 
     const init = () => {
         render();
